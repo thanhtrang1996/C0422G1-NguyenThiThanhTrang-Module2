45034d1fed98e4ad9e7b21da9463b16670c96a2e
unknown <you@example.com>
2022-06-16 20:03:18 +0700

    bt

--- src/ss7_abstraction_interface/bai_tap/colorable/Rectangle.java
+++ src/ss7_abstraction_interface/bai_tap/colorable/Rectangle.java
@@ -12,11 +12,6 @@ public class Rectangle extends Shape {
         this.wirth = wirth;
     }
 
-    public Rectangle(String color, boolean filled, int length, int wirth) {
-        super(color, filled);
-        this.length = length;
-        this.wirth = wirth;
-    }
 
     public int getArea() {
         return this.length*this.wirth;
---
+++ src/ss8_clear_code_refactoring/thuc_hanh/refactoring/FizzBuzz.java
@@ -0,0 +1,16 @@
+package ss8_clear_code_refactoring.thuc_hanh.refactoring;
+
+public class FizzBuzz {
+    public static String fizzBuzz(int number){
+        if(number % 3 == 0 && number % 5 == 0)
+            return "FizzBuzz";
+
+        if(number % 3 == 0)
+            return "Fizz";
+
+        if(number % 5 == 0)
+            return "Buzz";
+
+        return number + "";
+    }
+}
